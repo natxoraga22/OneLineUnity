0aa360db061fdff3aa920aeda3bb909149f73556
natxoraga22 <natxoraga22@gmail.com>
2018-08-04 21:52:24 +0200

    Wall collider for player scoring moved up

--- Assets/Scripts/Wall.cs
+++ Assets/Scripts/Wall.cs
@@ -6,6 +6,6 @@ public class Wall : MonoBehaviour {
 	void OnTriggerEnter2D(Collider2D other)
 	{
 		//if the player hits the trigger collider in between the walls then increment the score
-		GameController.current.PlayerScored ();
+		GameController.current.PlayerScored();
 	}
 }
