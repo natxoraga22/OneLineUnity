439963b201dffe3115d45bc5473a4f29e007dbeb
natxoraga22 <natxoraga22@gmail.com>
2018-08-05 03:48:39 +0200

    Unity Ads

--- Assets/Scripts/GameOverController.cs
+++ Assets/Scripts/GameOverController.cs
@@ -1,21 +1,32 @@
 ﻿using UnityEngine;
-using System.Collections;
 using UnityEngine.UI;
+using UnityEngine.Advertisements;
+
 
 public class GameOverController : MonoBehaviour {
-    
+
+    public AdsManager adsManager;
 	public Text scoreText;
 	public Text highScoreText;
     public Text newHighScoreText;
 
+    public int gameOversBetweenAds = 5;
+    private static int gameOverCounter = 0;
+
+
 	void Start () 
 	{
+        //update UI
         if (ScoreManager.instance) {
             scoreText.text = "" + ScoreManager.instance.GetScore();
             highScoreText.text = "" + ScoreManager.instance.GetHighScore();
             Color newHighTextScoreColor = new Color(1f, 1f, 1f, ScoreManager.instance.IsNewHighScore() ? 1f : 0f);
             newHighScoreText.color = newHighTextScoreColor;
         }
+
+        //show ad if needed
+        gameOverCounter++;
+        if (gameOverCounter % gameOversBetweenAds == 0 && Advertisement.IsReady()) Advertisement.Show();
 	}
 
 }
