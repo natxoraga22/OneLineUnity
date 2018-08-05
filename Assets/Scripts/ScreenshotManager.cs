89c1ecf49ed7ec2731a866bc659a1841567fa147
natxoraga22 <natxoraga22@gmail.com>
2018-08-05 06:19:25 +0200

    New Screenshot Manager

---
+++ Assets/Scripts/ScreenshotManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ScreenshotManager : MonoBehaviour {
+
+    public static ScreenshotManager instance = null;
+
+
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+        else if (instance != this) Destroy(gameObject);
+        DontDestroyOnLoad(gameObject);    
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S)) TakeScreenShot();
+    }
+
+    public static string ScreenShotName()
+    {
+        return string.Format("{0}/../Resources/Screenshots/screenshot_{1}.png",
+                             Application.dataPath,
+                             System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss"));
+    }
+
+    public void TakeScreenShot() {
+        ScreenCapture.CaptureScreenshot(ScreenshotManager.ScreenShotName());
+    }
+
+}
