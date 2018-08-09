e783321780d1663c60254a9c2ba2bcbee901e9b3
natxoraga22 <natxoraga22@gmail.com>
2018-08-09 19:48:52 +0200

    Leaderboard working finally!!!

--- Assets/GooglePlayGames/GameInfo.cs
+++ Assets/GooglePlayGames/GameInfo.cs
@@ -38,7 +38,7 @@ namespace GooglePlayGames {
 
         public const string ApplicationId = "302411567557"; // Filled in automatically
         public const string IosClientId = "__IOS_CLIENTID__"; // Filled in automatically
-        public const string WebClientId = ""; // Filled in automatically
+        public const string WebClientId = "302411567557-muh76elgvorf4egmufqjpspnfg4tees2.apps.googleusercontent.com"; // Filled in automatically
         public const string NearbyConnectionServiceId = "";
 
         public static bool ApplicationIdInitialized() {
--- Assets/Scripts/LeaderboardManager.cs
+++ Assets/Scripts/LeaderboardManager.cs
@@ -10,37 +10,44 @@ public class LeaderboardManager : MonoBehaviour {
 
     private void Awake()
     {
-        if (instance == null) instance = this;
+        Debug.Log("LeaderboardManager.Awake()");
+        if (instance == null) {
+            instance = this;
+            PlayGamesPlatform.DebugLogEnabled = true;
+            PlayGamesPlatform.Activate();
+        }
         else if (instance != this) Destroy(gameObject);
         DontDestroyOnLoad(gameObject); 
     }
 
-    public void Start()
-    {
-        PlayGamesPlatform.DebugLogEnabled = true;
-        PlayGamesPlatform.Activate();
-    }
-
     public void Authenticate() 
     {
+        Debug.Log("LeaderboardManager.Authenticate()");
         if (!Social.localUser.authenticated) {
+            Debug.Log("Not authenticated. Trying to");
             Social.localUser.Authenticate((bool success) => {
                 if (success) Debug.Log("AUTHENTICATION SUCCESS");
                 else Debug.Log("AUTHENTICATION FAILED");
             });
+            /*PlayGamesPlatform.Instance.Authenticate((bool success) => {
+                if (success) Debug.Log("AUTHENTICATION SUCCESS");
+                else Debug.Log("AUTHENTICATION FAILED");
+            });*/
         }
     }
 
     public void ShowLeaderboard() 
     {
+        Debug.Log("LeaderboardManager.ShowLeaderboard()");
         Social.ShowLeaderboardUI();
     }
 	
     public void PostScore(int score) 
     {
+        Debug.Log("LeaderboardManager.PostScore(" + score + ")");
         if (Social.localUser.authenticated) {
             Social.ReportScore(score, GPGSIds.leaderboard_leaderboard, (bool success) => {
-                Debug.Log("REPORT SCORE");
+                Debug.Log("REPORT SCORE " + score);
             });
         }
     }
