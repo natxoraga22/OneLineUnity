a803fe4dda6d3053b5a2927412a203e5e1ee1b49
natxoraga22 <natxoraga22@gmail.com>
2018-08-11 02:34:54 +0200

    Possibility to mute the music from main menu

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -7,12 +7,25 @@ public class AudioManager : MonoBehaviour {
 
     public static AudioManager instance = null;
 
+    private bool muted = false;
+
 
     private void Awake()
     {
         if (instance == null) instance = this;
         else if (instance != this) Destroy(gameObject);
         DontDestroyOnLoad(gameObject);
+    }
+
+    public bool IsMuted() 
+    {
+        return muted;
+    }
+
+    public void ToggleMute() 
+    {
+        muted = !muted;
+        gameObject.GetComponent<AudioSource>().mute = muted;
     }
 
 }
--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -1,17 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class MainMenuController : MonoBehaviour {
     
+    public Button muteButton;
+
+    public Sprite audioOnSprite;
+    public Sprite audioOffSprite;
+
+
 	private void Start() 
     {
         LeaderboardManager.instance.Authenticate();
+
+        if (AudioManager.instance.IsMuted()) muteButton.image.sprite = audioOffSprite;
+        else muteButton.image.sprite = audioOnSprite;
 	}
 
     public void ShowLeaderboard() 
     {
         LeaderboardManager.instance.ShowLeaderboard(GPGSIds.leaderboard_leaderboard);
+    }
+
+    public void ToggleMute() 
+    {
+        AudioManager.instance.ToggleMute();
+        if (AudioManager.instance.IsMuted()) muteButton.image.sprite = audioOffSprite;
+        else muteButton.image.sprite = audioOnSprite;
     }
 	
 }
